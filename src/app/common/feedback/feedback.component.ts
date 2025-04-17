9df20eb754f56524c2e076020a0c8f9bcc5214c5
Abidkhan732 <abid.khan@averlabz.com>
2025-04-17 12:25:45 +0500

    WetSide BAsic Setting for Home and about us almost done

--- src/app/common/feedback/feedback.component.ts
+++ src/app/common/feedback/feedback.component.ts
@@ -45,66 +45,66 @@ export class FeedbackComponent {
             }
         }
     }
-    feedbackSlides2: OwlOptions = {
-		nav: false,
-		margin: 25,
-		loop: true,
-		dots: true,
-		autoplay: true,
-		smartSpeed: 1000,
-		autoplayHoverPause: true,
-		navText: [
-            '<i class="flaticon-011-chevron-1"></i>',
-            '<i class="flaticon-010-chevron"></i>'
-		],
-        responsive: {
-            0: {
-                items: 1
-            },
-            515: {
-                items: 1
-            },
-            695: {
-                items: 2
-            },
-            935: {
-                items: 2
-            },
-            1200: {
-                items: 2
-            }
-        }
-    }
-    feedbackSlides3: OwlOptions = {
-		nav: false,
-		margin: 25,
-		loop: true,
-		dots: true,
-		autoplay: true,
-		smartSpeed: 1000,
-		autoplayHoverPause: true,
-		navText: [
-            '<i class="flaticon-011-chevron-1"></i>',
-            '<i class="flaticon-010-chevron"></i>'
-		],
-        responsive: {
-            0: {
-                items: 1
-            },
-            515: {
-                items: 1
-            },
-            695: {
-                items: 2
-            },
-            935: {
-                items: 2
-            },
-            1200: {
-                items: 3
-            }
-        }
-    }
+    // feedbackSlides2: OwlOptions = {
+	// 	nav: false,
+	// 	margin: 25,
+	// 	loop: true,
+	// 	dots: true,
+	// 	autoplay: true,
+	// 	smartSpeed: 1000,
+	// 	autoplayHoverPause: true,
+	// 	navText: [
+    //         '<i class="flaticon-011-chevron-1"></i>',
+    //         '<i class="flaticon-010-chevron"></i>'
+	// 	],
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         515: {
+    //             items: 1
+    //         },
+    //         695: {
+    //             items: 2
+    //         },
+    //         935: {
+    //             items: 2
+    //         },
+    //         1200: {
+    //             items: 2
+    //         }
+    //     }
+    // }
+    // feedbackSlides3: OwlOptions = {
+	// 	nav: false,
+	// 	margin: 25,
+	// 	loop: true,
+	// 	dots: true,
+	// 	autoplay: true,
+	// 	smartSpeed: 1000,
+	// 	autoplayHoverPause: true,
+	// 	navText: [
+    //         '<i class="flaticon-011-chevron-1"></i>',
+    //         '<i class="flaticon-010-chevron"></i>'
+	// 	],
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         515: {
+    //             items: 1
+    //         },
+    //         695: {
+    //             items: 2
+    //         },
+    //         935: {
+    //             items: 2
+    //         },
+    //         1200: {
+    //             items: 3
+    //         }
+    //     }
+    // }
     testimonialsSlides: OwlOptions = {
         items: 1,
 		nav: false,
