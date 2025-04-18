f7ca2b2384d1ee9a04d2014eac3fc0cdfb135deb
Abidkhan732 <abid.khan@averlabz.com>
2025-04-18 12:01:29 +0500

    update navbar details, baout and services

--- src/app/pages/dentist-details-page/dentist-details-page.component.ts
+++ src/app/pages/dentist-details-page/dentist-details-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DOCTOR_DATA, DoctorDetails } from '../../common/doctor-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-dentist-details-page',
-    imports: [RouterLink],
+    imports: [RouterLink,CommonModule],
     templateUrl: './dentist-details-page.component.html',
     styleUrls: ['./dentist-details-page.component.scss']
 })
-export class DentistDetailsPageComponent {}
+export class DentistDetailsPageComponent implements OnInit {
+    doctorName: string = '';
+    doctorDetails?: DoctorDetails;
+    constructor(private route: ActivatedRoute) {}
+  
+    ngOnInit() {
+      this.route.queryParams.subscribe(params => {
+        this.doctorName = params['name'] || '';
+        this.getDoctorDetails(this.doctorName);
+      });
+    }
+    getDoctorDetails(name: string) {
+      this.doctorDetails = DOCTOR_DATA.find(doc => doc.name === name);
+      debugger;
+    }
+}
