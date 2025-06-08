e7d912c396690020871255c0385417faa61053e3
Farhat Ullah <farhatkhanwazir@gmail.com>
2025-06-08 11:28:17 +0500

    environment.prod.ts and environment.ts added

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  baseUrl: "https://secp.khazanapk.com/api/",
+  production: true,
+   googlecapachakey:'6LeEGVgrAAAAAPs1Z9im9SlBfvHeg09CRP4UgNOX',
+  googlecapachasecret:'6LdhE1grAAAAAFVZGOuguCb3ZqwhYOBJ_JXZjkI8'
+};
